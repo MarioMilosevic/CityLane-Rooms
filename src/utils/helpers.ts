75b524d77d9cbce871252bc8b7b352d473a5c540
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-08-31 17:08:22 +0200

    Refactor updateRooms function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,67 +1,49 @@
 import { RoomType } from "../types/types";
 
-// export const sortRooms = (rooms: RoomType[], sortKey: string) => {
-//   const sortedRooms = [...rooms];
-//   switch (sortKey) {
-//     case "name (A-Z)":
-//       return sortedRooms.sort((a, b) => a.name.localeCompare(b.name));
-//     case "name (Z-A)":
-//       return sortedRooms.sort((a, b) => b.name.localeCompare(a.name));
-//     case "price (low first)":
-//       return sortedRooms.sort(
-//         (a, b) => Number(a.regularPrice) - Number(b.regularPrice)
-//       );
-//     case "price (high first)":
-//       return sortedRooms.sort(
-//         (a, b) => Number(b.regularPrice) - Number(a.regularPrice)
-//       );
-//     case "capacity (low first)":
-//       return sortedRooms.sort(
-//         (a, b) => Number(a.capacity) - Number(b.capacity)
-//       );
-//     case "capacity (high first)":
-//       return sortedRooms.sort(
-//         (a, b) => Number(b.capacity) - Number(a.capacity)
-//       );
-//     default:
-//       return sortedRooms;
-//   }
-// };
-
-export const updateRooms = (rooms, filter, sort) => {
-  console.log('uslo')
+export const updateRooms = (
+  rooms: RoomType[],
+  filter: string,
+  sort: string
+): RoomType[] => {
   let roomsCopy = [...rooms];
-  if (filter === "All rooms") {
-    roomsCopy = [...rooms];
-  }
-  if (filter === "No discount") {
-    roomsCopy = roomsCopy.filter((room) => room.discount === 0);
+
+  switch (filter) {
+    case "No discount":
+      roomsCopy = roomsCopy.filter((room) => room.discount === 0);
+      break;
+    case "With discount":
+      roomsCopy = roomsCopy.filter((room) => room.discount > 0);
+      break;
+    case "All rooms":
+    default:
+      break;
   }
 
-  if (filter === "With discount") {
-    roomsCopy = roomsCopy.filter((room) => room.discount > 0);
-  }
-  if (sort === "name (A-Z)") {
-    roomsCopy.sort((a, b) => a.name.localeCompare(b.name));
-  }
-  if (sort === "name (Z-A)") {
-    roomsCopy.sort((a, b) => b.name.localeCompare(a.name));
-  }
-  if (sort === "price (low first)") {
-    roomsCopy.sort((a, b) => Number(a.regularPrice) - Number(b.regularPrice));
-  }
-  if (sort === "price (high first)") {
-    roomsCopy.sort((a, b) => Number(b.regularPrice) - Number(a.regularPrice));
-  }
-  if (sort === "capacity (low first)") {
-    roomsCopy.sort((a, b) => Number(a.capacity) - Number(b.capacity));
-  }
-  if (sort === "capacity (high first)") {
-    roomsCopy.sort((a, b) => Number(b.capacity) - Number(a.capacity));
+  // Apply sort using switch
+  switch (sort) {
+    case "name (A-Z)":
+      roomsCopy.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case "name (Z-A)":
+      roomsCopy.sort((a, b) => b.name.localeCompare(a.name));
+      break;
+    case "price (low first)":
+      roomsCopy.sort((a, b) => Number(a.regularPrice) - Number(b.regularPrice));
+      break;
+    case "price (high first)":
+      roomsCopy.sort((a, b) => Number(b.regularPrice) - Number(a.regularPrice));
+      break;
+    case "capacity (low first)":
+      roomsCopy.sort((a, b) => Number(a.capacity) - Number(b.capacity));
+      break;
+    case "capacity (high first)":
+      roomsCopy.sort((a, b) => Number(b.capacity) - Number(a.capacity));
+      break;
+    default:
+      // If no valid sort option is provided, return as is
+      break;
   }
 
   return roomsCopy;
 };
-
-
 
