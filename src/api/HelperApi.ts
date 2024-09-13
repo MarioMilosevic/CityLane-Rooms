f699cc5194440b4d53c3287f01e2f6d0b9836314
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-09-13 16:21:28 +0200

    Add helper

---
+++ src/api/HelperApi.ts
@@ -0,0 +1,146 @@
+import supabase from "src/config/supabaseClient";
+import { isFuture, isPast, isToday } from "date-fns";
+import { subtractDates } from "src/utils/helpers";
+import { rooms } from "src/data/data-rooms";
+import { bookings } from "src/data/data-bookings";
+import { guests } from "src/data/data-guests";
+
+export const deleteGuests = async () => {
+  try {
+    const { error } = await supabase.from("Guests").delete().gt("id", 0);
+    if (error) console.log(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const deleteRooms = async () => {
+  try {
+    const { error } = await supabase.from("Rooms").delete().gt("id", 0);
+    if (error) console.log(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const deleteBookings = async () => {
+  try {
+    const { error } = await supabase.from("Bookings").delete().gt("id", 0);
+    if (error) console.log(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const createGuests = async () => {
+  try {
+    const { error } = await supabase.from("Guests").insert(guests);
+    if (error) console.log(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const createRooms = async () => {
+  try {
+    const { error } = await supabase.from("Rooms").insert(rooms);
+    if (error) console.log(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const createBookings = async () => {
+  try {
+    const { data: guestsIds } = await supabase
+      .from("Guests")
+      .select("id")
+      .order("id");
+
+    const allGuestIds = guestsIds?.map((guest) => guest.id);
+
+    const { data: roomsIds } = await supabase
+      .from("Rooms")
+      .select("id")
+      .order("id");
+    const allroomIds = roomsIds?.map((room) => room.id);
+
+    const finalBookings = bookings.map((booking) => {
+      const room = rooms.at(booking.roomId - 1);
+      if (!room) return;
+      const numNights = subtractDates(booking.endDate, booking.startDate);
+      const roomPrice = numNights * (room.regularPrice - room.discount);
+      const extrasPrice = booking.hasBreakfast
+        ? numNights * 10 * booking.numGuests
+        : 0;
+      const totalPrice = roomPrice + extrasPrice;
+
+      let status;
+      if (
+        isPast(new Date(booking.endDate)) &&
+        !isToday(new Date(booking.endDate))
+      )
+        status = "checked-out";
+      if (
+        isFuture(new Date(booking.startDate)) ||
+        isToday(new Date(booking.startDate))
+      )
+        status = "unconfirmed";
+      if (
+        (isFuture(new Date(booking.endDate)) ||
+          isToday(new Date(booking.endDate))) &&
+        isPast(new Date(booking.startDate)) &&
+        !isToday(new Date(booking.startDate))
+      )
+        status = "checked-in";
+
+      return {
+        ...booking,
+        numNights,
+        roomPrice,
+        extrasPrice,
+        totalPrice,
+        guestId: allGuestIds?.at(booking.guestId - 1),
+        roomId: allroomIds?.at(booking.roomId - 1),
+        status,
+      };
+    });
+
+    const { error } = await supabase.from("Bookings").insert(finalBookings);
+    if (error) console.error(error.message);
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const uploadAll = async () => {
+  try {
+    await deleteBookings();
+    await deleteGuests();
+    await deleteRooms();
+    await createGuests();
+    await createRooms();
+    await createBookings();
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
+
+export const uploadBookings = async () => {
+  try {
+    await deleteBookings();
+    await createBookings();
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
